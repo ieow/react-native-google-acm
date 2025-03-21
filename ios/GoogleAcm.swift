8dbe1cb369e4b4d583a5439839bfcc2edb284867
ieow <cwlee0@gmail.com>
2025-03-21 12:58:49 +0800

    chore: initial commit

---
+++ ios/GoogleAcm.swift
@@ -0,0 +1,8 @@
+@objc(GoogleAcm)
+class GoogleAcm: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
